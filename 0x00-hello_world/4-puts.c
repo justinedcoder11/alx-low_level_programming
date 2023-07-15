b455085ad2a456572ff8ce3902bd4101d36dd6cb
Justine Gbabo <justinegbabo1@gmail.com>
2023-07-14 17:26:33 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
+ * A program which display by using puts
  * main = entry point
  *
  * return: always 0 (success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
